0366402c1ffb8b665fd7561269b6c91ece9eb569
Sophia Elisa <sophiadiazinfante@gmail.com>
2015-09-05 14:57:48 -0700

    added my html file to git

---
+++ resources/javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  //$("#copyright").click(function(){
+//    $(this).css('color','purple');
+//  });
+document.getElementById('copyright').onclick = function () {
+  this.style.color = 'purple';
+}
+});
